40e69222f9adee72034504db3f5823c61e40dfdd
Joao Berardo <jberardo@me.com>
2019-03-08 17:45:23 -0800

    Initializing Spring Boot Project

---
+++ studentdal/studentdal/src/main/java/io/joca/studentdal/DemoApplication.java
@@ -0,0 +1,15 @@
+package io.joca.studentdal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Joao Berardo
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
