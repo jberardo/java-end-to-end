8b2159ab0628c070a47ed15125314fd55be36c4d
Joao Berardo <jberardo@me.com>
2019-03-09 03:22:53 -0800

    implemented Find Flights

---
+++ flightreservation/src/main/java/io/joca/flightreservation/controllers/FlightController.java
@@ -0,0 +1,32 @@
+package io.joca.flightreservation.controllers;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import io.joca.flightreservation.repositories.FlightRepository;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class FlightController {
+
+    @Autowired
+    private FlightRepository flightRepository;
+
+    @PostMapping("/findFlight")
+    public String findFlights(
+        @RequestParam("from") String from,
+        @RequestParam("to") String to,
+        @RequestParam("departureDate") @DateTimeFormat(pattern = "MM-dd-yyyy") LocalDate departureDate, Model model) {
+        
+        log.info("Searching flights");
+        model.addAttribute("flights", flightRepository.findFlights(from, to, departureDate));
+        return "displayFlights";
+    }
+}
