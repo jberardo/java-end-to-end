8a2a54be122a8b297102faa800c5bb2b86dc6f08
Joao Berardo <jberardo@me.com>
2019-03-09 02:02:48 -0800

    adding method to save user registratopn and login view

--- flightreservation/src/main/java/io/joca/flightreservation/controllers/UserController.java
+++ flightreservation/src/main/java/io/joca/flightreservation/controllers/UserController.java
@@ -1,17 +1,31 @@
 package io.joca.flightreservation.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import io.joca.flightreservation.entities.User;
+import io.joca.flightreservation.repositories.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/showReg")
     public String showRegistrationPage() {
         log.info("showReg called");
         return "login/registerUser";
+    }
+
+    @PostMapping("/registerUser")
+    public String register(@ModelAttribute("User") User user) {
+        userRepository.save(user);
+        return "login/login";
     }
 }
