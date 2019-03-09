c56a47c017566d4d55c86d517726abe1a919073a
Joao Berardo <jberardo@me.com>
2019-03-09 04:04:27 -0800

    created Reservation Service

---
+++ flightreservation/src/main/java/io/joca/flightreservation/services/ReservationServiceImpl.java
@@ -0,0 +1,47 @@
+package io.joca.flightreservation.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import io.joca.flightreservation.dto.ReservationRequest;
+import io.joca.flightreservation.entities.Flight;
+import io.joca.flightreservation.entities.Passenger;
+import io.joca.flightreservation.entities.Reservation;
+import io.joca.flightreservation.repositories.FlightRepository;
+import io.joca.flightreservation.repositories.PassengerRepository;
+import io.joca.flightreservation.repositories.ReservationRepository;
+
+public class ReservationServiceImpl implements ReservationService {
+
+    @Autowired
+    private FlightRepository flightRepository;
+
+    @Autowired
+    private PassengerRepository passengerRepository;
+
+    @Autowired
+    private ReservationRepository ReservationRepository;
+
+    @Override
+    public Reservation bookFlight(ReservationRequest request) {
+        // make payment
+
+        Long flightId = request.getFlightId();
+        Flight flight = flightRepository.findById(flightId).orElse(null);
+
+        Passenger passenger = new Passenger();
+        passenger.setFirstName(request.getPassengerFirstName());
+        passenger.setLastMame(request.getPassengerLasttName());
+        passenger.setEmail(request.getPassengerEmail());
+        passenger.setPhone(request.getPassengerPhone());
+        Passenger savedPassenger = passengerRepository.save(passenger);
+
+        Reservation reservation = new Reservation();
+        reservation.setFlight(flight);
+        reservation.setPassenger(savedPassenger);
+        reservation.setCheckedIn(false);
+
+        Reservation savedReservation = ReservationRepository.save(reservation);
+
+        return savedReservation;
+    }
+}
