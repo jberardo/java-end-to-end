ad77fffda702357adb1a3573b0aeb35136886f00
Joao Berardo <jberardo@me.com>
2019-03-09 02:01:33 -0800

    updating Flight to extend from AbstractEntity

--- flightreservation/src/main/java/io/joca/flightreservation/entities/Flight.java
+++ flightreservation/src/main/java/io/joca/flightreservation/entities/Flight.java
@@ -4,9 +4,6 @@ import java.sql.Timestamp;
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,12 +15,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Flight {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+public class Flight extends AbstractEntity {
     private String flightNumber;
     private String operating_airlines;
     private String departure_city;
