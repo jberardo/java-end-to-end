f987a50703fc18bc494c994b2def775304982231
Joao Berardo <jberardo@me.com>
2019-03-10 12:47:35 -0700

    implemented UserDetailsService

--- flightreservation/src/main/java/io/joca/flightreservation/entities/Role.java
+++ flightreservation/src/main/java/io/joca/flightreservation/entities/Role.java
@@ -5,6 +5,8 @@ import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
 
+import org.springframework.security.core.GrantedAuthority;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -15,9 +17,17 @@ import lombok.ToString;
 @RequiredArgsConstructor
 @ToString
 @Entity
-public class Role extends AbstractEntity {
+public class Role extends AbstractEntity implements GrantedAuthority {
+
+    private static final long serialVersionUID = -7997550770731562327L;
+
     private String name;
 
     @ManyToMany(mappedBy = "roles")
     private Set<User> users;
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
 }
---
+++ flightreservation/src/main/java/io/joca/flightreservation/services/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package io.joca.flightreservation.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import io.joca.flightreservation.entities.User;
+import io.joca.flightreservation.repositories.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found: " + username);
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), user.getRoles());
+    }
+}
