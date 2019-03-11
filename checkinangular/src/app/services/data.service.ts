85ec51f2a221f061246c2df544160811172e7d0d
Joao Berardo <jberardo@me.com>
2019-03-10 17:24:39 -0700

    implementing data service

--- checkinangular/src/app/services/data.service.ts
+++ checkinangular/src/app/services/data.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  url:string = 'http://localhost:8085/reservations/';
+
+  constructor(private _http:HttpClient) { }
+
+  public getReservation(id:number):any {
+    return this._http.get(this.url+id);
+  }
+
+  public checkIn(checkInRequest):any {
+    return this._http.post(this.url, checkInRequest);
+  }
 }
