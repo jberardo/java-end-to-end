59568f5df7a8b0cbf8ef1c9a3d46268fad15aa61
Joao Berardo <jberardo@me.com>
2019-03-10 17:24:18 -0700

    adding imports

--- checkinangular/src/app/app.module.ts
+++ checkinangular/src/app/app.module.ts
@@ -7,6 +7,10 @@ import { StartcheckinComponent } from './components/startcheckin/startcheckin.co
 import { CheckinComponent } from './components/checkin/checkin.component';
 import { ConfirmCheckInComponent } from './components/confirm-check-in/confirm-check-in.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { DataService } from './services/data.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,9 +20,11 @@ import { ConfirmCheckInComponent } from './components/confirm-check-in/confirm-c
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
