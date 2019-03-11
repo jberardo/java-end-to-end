0d95bb04beb4f710e975ab3db25eefd26c0aec4e
Joao Berardo <jberardo@me.com>
2019-03-10 17:24:29 -0700

    adding routing

---
+++ checkinangular/src/app/routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from "@angular/core";
+import {Routes, RouterModule} from '@angular/router';
+import { StartcheckinComponent } from './components/startcheckin/startcheckin.component';
+import { CheckinComponent } from './components/checkin/checkin.component';
+import { ConfirmCheckInComponent } from './components/confirm-check-in/confirm-check-in.component';
+
+const routes:Routes=[
+    {
+        path: '',
+        redirectTo: '',
+        pathMatch: 'full'
+    },
+    {
+        path: 'startCheckIn',
+        component: StartcheckinComponent
+    },
+    {
+        path: 'checkIn/:id',
+        component: CheckinComponent
+    },
+    {
+        path: 'confirm',
+        component: ConfirmCheckInComponent
+    }
+];
+
+@NgModule({
+    imports:[RouterModule.forRoot(routes)],
+    exports:[RouterModule]
+})
+export class AppRouterModule {
+
+}
